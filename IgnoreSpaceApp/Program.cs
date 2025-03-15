4093c7e5e3c8d7b577c9d42638f9ae30108b2d43
Siberian <vasileios.kritikos@aueb.gr>
2025-03-15 19:41:38 +0200

    Ignore Spaces App

---
+++ IgnoreSpaceApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace IgnoreSpaceApp;
+/// <summary>
+/// Ignores spaces
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        int codeRead = 0;
+
+        do
+        {
+            codeRead = Console.Read();
+            if (codeRead != 0)
+            {
+                if (codeRead != 32)
+                {
+                    Console.Write($"{(char)codeRead}");
+                }
+            }
+        } while (codeRead != 10);
+    }
+}
