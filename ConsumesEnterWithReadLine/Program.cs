b1a1d547a20c13100e3ac4bbe01eda0ec04c8cca
Siberian <vasileios.kritikos@aueb.gr>
2025-03-15 20:10:41 +0200

    Consumes Enter With ReadLine

---
+++ ConsumesEnterWithReadLine/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsumesEnterWithReadLine;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char inputChar = ' ';
+
+        do
+        {
+            Console.Write("Please insert a char:\n ");
+            inputChar = (char) Console.Read();
+            Console.ReadLine();
+            Console.WriteLine($"You inserted char: \t{inputChar}\n");
+        } while(inputChar != '*');
+        Console.WriteLine("Bye");
+    }
+}
