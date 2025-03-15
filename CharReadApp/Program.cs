8f7ffab4849b5884896b5ea4ac79c890773642a9
Siberian <vasileios.kritikos@aueb.gr>
2025-03-15 19:36:40 +0200

    Console Read App

---
+++ CharReadApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CharReadApp;
+/// <summary>
+/// Reads characters using Console.Read()
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        char inputChar;
+        int charCode;
+
+        do
+        {
+            charCode = Console.Read();
+            inputChar = (char) charCode;
+            Console.WriteLine($"char: {inputChar} -- charCode: {charCode}");
+            
+        }while (charCode != 10);
+    }
+}
