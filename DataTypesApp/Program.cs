03f6f708a093d97071f3efe0e09fa9c85bd2afdf
Siberian <vasileios.kritikos@aueb.gr>
2025-03-15 19:22:24 +0200

    first commit

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml;
+
+namespace DataTypesApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 5;
+        switch (number)
+        {
+            case 1:
+            case 4:
+            case 6:
+            case 8:
+            case 10:
+                Console.WriteLine("The number is not prime!");
+                break;
+            case 2:
+            case 5:
+            case 7:
+                Console.WriteLine("The number is prime!"); 
+                break;
+            default: Console.WriteLine("Unknown number!");
+                break;
+        }
+    }
+}
