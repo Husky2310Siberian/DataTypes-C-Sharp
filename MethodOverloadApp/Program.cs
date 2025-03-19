9c7e2bdce3754c1397f8664b346fa16bde2e9cd8
Siberian <vasileios.kritikos@aueb.gr>
2025-03-19 13:01:52 +0200

    Methods overload

---
+++ MethodOverloadApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MethodOverloadApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int b = 2;
+        float c = 10F;
+        float d = 20F;
+        
+        PrintNums(a,c);
+        PrintNums(c,d);
+    }
+
+    public static void PrintNums(int intVal, float floatVal)
+    {
+        Console.WriteLine(intVal +" , "+ floatVal);
+    }
+
+    public static void PrintNums(float intVal, float floatVal)
+    {
+        Console.WriteLine(intVal + " , "+ floatVal);
+    }
+}
