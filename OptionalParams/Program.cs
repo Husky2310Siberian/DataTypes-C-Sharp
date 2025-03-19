81d4dd0c20fe516d41bdddaa3a8ed09f05a985c9
Siberian <vasileios.kritikos@aueb.gr>
2025-03-19 12:53:53 +0200

    Date time app

---
+++ OptionalParams/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OptionalParams;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime date1 = GetDateTime();
+        Console.WriteLine($"{date1:D}");
+        
+        DateTime date2 = GetDateTime();
+        Console.WriteLine($"{date2:F}");
+        
+    }
+
+    public static DateTime GetDateTime(int year=2025, int month=3, int day=18, int hour=0, int minute=0, int second=0, int millisecond=0)
+    {
+      return new DateTime(year, month, day, hour, minute, second, millisecond , DateTimeKind.Utc);
+    }
+}
